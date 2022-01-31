61833ea82d245c31df1e4fc2e979e7b8ecfe2e32
Lucas Henrique <lucashenriquesd@gmail.com>
2022-01-31 16:28:11 -0300

    added user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -17,7 +18,7 @@ import { UsersModule } from './users/users.module';
       username: process.env.POSTGRES_USERNAME,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
-      entities: [],
+      entities: [User],
       synchronize: process.env.SYNCHRONIZE === 'true',
     }),
     AuthModule,
